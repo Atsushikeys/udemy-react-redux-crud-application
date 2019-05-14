d530b2da9fc9c59083fd86536f9c12065346a0c7
atsushikeys <atsushi.dans.keyboard@gmail.com>
2019-05-14 22:05:02 +0900

    implement action and action creator

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+const INCREMENT = 'INCREMENT'
+const DECREMENT = 'DECREMENT'
+
+export const increment = () => ({
+    type: 'INCRIMENT'
+  })
+
+
+export const decrement = () => ({
+  type: 'DECRIMENT'
+})
